fe641cea54ca4decc865e5e6e70e7e8f81f92ec1
vai <v.shah@stok.ly>
2026-04-07 23:31:01 +0100

    selected

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,7 +1,7 @@
 <select name="{{ $name }}" id="{{ $name }}" class="form-select @error($name) is-invalid @enderror">
 	<option value="">Select {{ strtolower($label) }}</option>
 	@foreach ($options as $text)
-		<option value="{{ $text }}" {{ old($name, $selected) == $text ? 'selected' : '' }}>
+		<option value="{{ $text }}" {{ $selected == $text ? 'selected' : '' }}>
 			{{ $text }}
 		</option>
 	@endforeach
