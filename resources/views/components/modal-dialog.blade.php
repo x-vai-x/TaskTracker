0a450ed754496e17a1338e7e658bdec5294a7bd3
vai <v.shah@stok.ly>
2026-04-08 13:21:24 +0100

    fixed id so modal now works

--- resources/views/components/modal-dialog.blade.php
+++ resources/views/components/modal-dialog.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="{{ '#' . $id }}" tabindex="-1">
+<div class="modal fade" id="{{ $id }}" tabindex="-1">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header bg-primary text-white">
