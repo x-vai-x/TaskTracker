f9413ca14b6b81fd382ea28dfcf8e49aa9409f64
vai <v.shah@stok.ly>
2026-04-09 22:51:40 +0100

    missed file

--- database/migrations/2026_04_02_213120_create_tasks_table.php
+++ database/migrations/2026_04_02_213120_create_tasks_table.php
@@ -19,9 +19,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+  
     public function down(): void
     {
         Schema::dropIfExists('tasks');
