a78aec3ee0ae84ae55ba221a9e8dc89b32bb59ad
vai <v.shah@stok.ly>
2026-04-10 00:30:30 +0100

    updated now works

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -2,16 +2,8 @@
 
 namespace App\Http\Requests;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-use App\Http\Requests\CreateTaskRequest;
 
->>>>>>> a1a386e (works)
-=======
 use App\Http\Requests\CreateTaskRequest;
-
->>>>>>> debug
 
 class UpdateTaskRequest extends CreateTaskRequest
 {
